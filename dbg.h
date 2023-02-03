1fd8c9ee918002ea4777fbcec8ddf7402def2b7f
lee-emqx <lihj@emqx.io>
2023-02-03 16:39:54 +0800

    * MDF [dbg.h] set default debug mode is OFF.

--- dbg.h
+++ dbg.h
@@ -6,10 +6,19 @@
 #include <string.h>
 
 
+#define NDEBUG
+#define NLOG
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
 #define debug(M, ...) fprintf(stderr, "[DEBUG] %s:%d: " M "\n",\
+		__FILE__, __LINE__, ##__VA_ARGS__)
+#endif
+
+#ifdef NLOG
+#define log_info(M, ...)
+#else
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n",\
 		__FILE__, __LINE__, ##__VA_ARGS__)
 #endif
 
@@ -22,9 +31,6 @@
 #define log_warn(M, ...) fprintf(stderr,\
 		"[WARN] (%s:%d: errno: %s) " M "\n",\
 		__FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
-
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n",\
-		__FILE__, __LINE__, ##__VA_ARGS__)
 
 #define check(A, M, ...) if(!(A)) {\
 	log_err(M, ##__VA_ARGS__); errno=0; goto error; }
