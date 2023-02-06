8361afc03bb63bd21ea43dba0ac4fac14713832f
lee-emqx <lihj@emqx.io>
2023-02-06 12:12:20 +0800

    * NEW [idl_serial] support for non nested json to struct.

--- idl_serial.c
+++ idl_serial.c
@@ -8,16 +8,29 @@
 
 extern FILE *yyin;
 
-static char func_head[] =
+static char ser_func_head[] =
 	"\ncJSON *dds_to_mqtt_%s_convert(%s *st)"
 	"\n{\n"
 	"\n 	cJSON *obj     = NULL;"
 	"\n 	/* Assemble cJSON* obj with *st. */"
 	"\n 	obj = cJSON_CreateObject();\n";
 
-static char func_tail[] =
+static char ser_func_tail[] =
 	"\n\treturn obj;"
 	"\n}\n\n";
+
+
+static char deser_func_head[] =
+	"\n%s *mqtt_to_dds_%s_convert(cJSON *jso)"
+	"\n{"
+	"\n\t%s *st = (%s) malloc(sizeof(*st));"
+	//TODO alloc memory
+	"\n\tcJSON *item = NULL;\n";
+
+static char deser_func_tail[] =
+	"\n\treturn st;"
+	"\n}\n\n";
+
 
 const char *cJSON_Add(char *key)
 {
@@ -34,6 +47,85 @@ const char *cJSON_Add(char *key)
 		return "Unsupport now";
 	}
 }
+
+const char *cJSON_Get(char *key)
+{
+	if (0 == strcmp(key, "BOOLEAN"))
+	{
+		return "";
+	}
+	else if (0 == strcmp(key, "NUMBER"))
+	{
+		return "";
+	}
+	else
+	{
+		return "Unsupport now";
+	}
+}
+
+int idl_serial_generator_to_json(cJSON *jso)
+{
+	cJSON *arrs = jso;
+	cJSON *arr = NULL;
+	cJSON *eles = NULL;
+	cJSON *ele = NULL;
+	cJSON *e = NULL;
+
+	cJSON_ArrayForEach(arr, arrs)
+	{
+		cJSON_ArrayForEach(eles, arr)
+		{
+
+			printf(ser_func_head, eles->string, eles->string);
+
+			cJSON_ArrayForEach(ele, eles)
+			{
+				cJSON_ArrayForEach(e, ele)
+				{
+					printf("\t%s(obj, \"%s\", st->%s);\n", cJSON_Add(e->string), e->valuestring, e->valuestring);
+				}
+			}
+
+			puts(ser_func_tail);
+		}
+	}
+
+	return 0;
+}
+
+int idl_serial_generator_to_struct(cJSON *jso)
+{
+	cJSON *arrs = jso;
+	cJSON *arr = NULL;
+	cJSON *eles = NULL;
+	cJSON *ele = NULL;
+	cJSON *e = NULL;
+
+	cJSON_ArrayForEach(arr, arrs)
+	{
+		cJSON_ArrayForEach(eles, arr)
+		{
+
+			printf(deser_func_head, eles->string, eles->string, eles->string, eles->string);
+
+			cJSON_ArrayForEach(ele, eles)
+			{
+				cJSON_ArrayForEach(e, ele)
+				{
+					printf("\titem = cJSON_GetObjectItem(obj, \"%s\");\n", e->valuestring);
+					printf("\tst->%s = item->valuedouble;\n", e->valuestring);
+				}
+			}
+
+			puts(deser_func_tail);
+		}
+	}
+
+	return 0;
+
+}
+
 
 int idl_serial_generator(const char *file)
 {
@@ -53,30 +145,8 @@ int idl_serial_generator(const char *file)
 	}
 	puts(cJSON_PrintUnformatted(jso));
 
-	cJSON *arrs = jso;
-	cJSON *arr = NULL;
-	cJSON *eles = NULL;
-	cJSON *ele = NULL;
-	cJSON *e = NULL;
-
-	cJSON_ArrayForEach(arr, arrs)
-	{
-		cJSON_ArrayForEach(eles, arr)
-		{
-
-			printf(func_head, eles->string, eles->string);
-
-			cJSON_ArrayForEach(ele, eles)
-			{
-				cJSON_ArrayForEach(e, ele)
-				{
-					printf("\t%s(obj, \"%s\", st->%s);\n", cJSON_Add(e->string), e->string, e->valuestring);
-				}
-			}
-
-			puts(func_tail);
-		}
-	}
+	idl_serial_generator_to_json(jso);
+	idl_serial_generator_to_struct(jso);
 
 	return 0;
 }
